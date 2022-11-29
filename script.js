2ed2d598129cdb12a187da4ada3fb64630d09ac0
Regina Varybrus <varybrusr@gmail.com>
2022-11-29 23:59:58 +0000

    homework done

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let someNumber = 123456;
+let someString = 'IT School `Hillel`';
+let someBoolean = false;
+
+console.log('value:', someNumber, 'type:', typeof someNumber);
+console.log('value:', someString, 'type:', typeof someString);
+console.log('value:', someBoolean, 'type:', typeof someBoolean);
+
+alert('Hello JS');
